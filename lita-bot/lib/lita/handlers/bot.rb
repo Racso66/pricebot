101d97e2a36c751641f197a52d2cfd8eafc47b9b
[noorkhan] <[noorkhan_92@hotmail.com]>
2020-12-19 12:26:14 +0500

    [this is initial repo]

---
+++ lita-bot/lib/lita/handlers/bot.rb
@@ -0,0 +1,26 @@
+module Lita
+  module Handlers
+    class Bot < Handler
+      route(
+        /^Hi/i,
+        :customer
+      )
+
+      route(
+        /i want a/i,
+        :negotiate
+      )
+
+      def customer(response)
+        response.reply("Hi, this is store agent. how may i help you!")
+      end
+
+      def negotiate(response)
+        #here it will respont with list of requested product.
+        response.reply("we have these brands, which one do you want please!")
+      end
+
+      Lita.register_handler(self)
+    end
+  end
+end
